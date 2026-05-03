d0e9b7cb5651151c71d0509cfc3de15e2e7d7eec
Angelo Urtula <aurtula1@fau.edu>
2026-05-03 01:33:13 -0400

    added login route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import './App.css';
 import { useRoutes } from 'react-router-dom';
 import { Link } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   let element = useRoutes([
     {
       path: "/",
       element: <Home />
+    },
+    {
+      path: "/login",
+      element: <Login/>
     }
   ]);
 
