d77c9c6688448022de191997f3e850a621c45c9a
Angelo Urtula <aurtula1@fau.edu>
2026-05-03 05:43:33 -0400

    Users can only edit their own posts

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import CommentBox from '../components/CommentBox'
+import { useAuth } from '../context/AuthContext'
 
 const PostPage = () => {
+    const { user } = useAuth()
     const { id } = useParams()
     const [post, setPost] = useState({})
     const [comments, setComments] = useState([])
@@ -47,7 +49,7 @@ const PostPage = () => {
             <h3>Content: {post.content}</h3>
             <h3>Image_url: {post.image_url}</h3>
             <button onClick={updateUpvotes}>{post.upvotes} Likes 👍</button>
-            <Link to={'/edit/' + post.id} state={{ title: post.title, content: post.content, image_url: post.image_url }}>Edit Your Post</Link>
+            {user?.id === post.user_id? (<Link to={'/edit/' + post.id} state={{ title: post.title, content: post.content, image_url: post.image_url }}>Edit Your Post</Link>):(null)}
             {comments && comments.length > 0 ? (
                 [...comments]
                     .sort((a, b) => {
