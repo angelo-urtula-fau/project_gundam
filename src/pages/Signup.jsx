e4419b41cd4a40d9a4eb93b77d42e50b042c9ac5
Angelo Urtula <aurtula1@fau.edu>
2026-05-03 06:46:57 -0400

    users can signup by providing an email and password

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import { supabase } from "../client"
+import { useNavigate } from "react-router-dom"
+
 const SignUp = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    const handleSignUp = async (e) => {
+        e.preventDefault()
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password
+        })
+        if (error) {
+            console.error("Error signing up:", error)
+        } else {
+            console.log("User signed up:", data)
+            navigate("/login")
+        }
+    }
+
     return (
         <div>
             <h1>Sign Up</h1>
+            <form onSubmit={handleSignUp}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Sign Up</button>
+            </form>
         </div>
     )
 }
