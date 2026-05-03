a968cca2fb615dc7c4b17c20ec7bb32f72dba4e0
Angelo Urtula <aurtula1@fau.edu>
2026-05-02 23:44:09 -0400

    barebones post form to insert to supabase

---
+++ src/components/CreatePostForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { supabase } from '../client';
+const CreatePost = () => {
+    const [post, setPost] = useState({ title: "", content: "", image_url: "" })
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setPost((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            }
+        })
+    }
+
+    const createPost = async (event) => {
+        await supabase
+            .from('posts')
+            .insert({
+                title: post.title,
+                content: post.content,
+                image_url: post.image_url
+            })
+            .select();
+        window.location = "/";
+    }
+
+    return (
+        <div>
+            <form>
+                <label htmlFor="title">Title</label> <br />
+                <input type="text" id="title" name="title" onChange={handleChange} /><br />
+                <br />
+                <label htmlFor="content">content</label><br />
+                <textarea rows="5" cols="50" id="content" name="content" onChange={handleChange}></textarea>
+                <br />
+                <label htmlFor="image_url">Image URL</label><br />
+                <input type="text" id="image)_url" name="image_url" onChange={handleChange} />
+
+                <br />
+                <input type="submit" value="Submit" onClick={createPost} />
+            </form>
+        </div>
+    )
+}
+
+export default CreatePost
