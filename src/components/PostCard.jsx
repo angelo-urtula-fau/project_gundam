42bf010b03fbb75dd06ccb8ce31c5de4b4108d00
Angelo Urtula <aurtula1@fau.edu>
2026-05-03 03:09:17 -0400

    auto indent

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,14 +1,16 @@
-import {useState} from 'react'
-import {supabase} from '../client'
-import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import { supabase } from '../client'
+import { Link } from 'react-router-dom'
 
 const PostCard = (props) => {
-    return(
-        <div className="PostCard">
-            <h1>Title: {props.title}</h1>
-            <h3>Posted at: {props.time}</h3>
-            <h3>Upvotes: {props.upvotes}</h3>
-        </div>
+    return (
+        
+            <div className="PostCard">
+                <h1>Title: {props.title}</h1>
+                <h3>Posted at: {props.time}</h3>
+                <h3>Upvotes: {props.upvotes}</h3>
+            </div>
+      
     )
 }
 
