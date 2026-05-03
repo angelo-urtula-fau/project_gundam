3708ee01f73de533996fff8cc9aae54398a4177c
Angelo Urtula <aurtula1@fau.edu>
2026-05-03 00:52:05 -0400

    corrected filename

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import {supabase} from '../client'
-import {Link} from 'react-router'
+import {Link} from 'react-router-dom'
 
 const PostCard = (props) => {
     return(
